e87f47f7ef3fa9016eb4c6c66d819d411fe69361
John Haiducek <jhaiduce@picard.local>
2018-11-16 15:22:04 -0500

    Add a transformed_kde class which computes KDE's in other than linear space

--- substorm_utils/kde.py
+++ substorm_utils/kde.py
@@ -1,10 +1,22 @@
 from scipy.stats import gaussian_kde
 import numpy as np
 
-def get_kde_bootstrap(data,evalpoints,nsamples,bw):
+class transformed_kde(object):
+
+    def __init__(self,dataset,bw_method=None,transform=lambda x: x, Dtrans=lambda x: 1):
+    
+        self.kde=gaussian_kde(transform(dataset),bw_method)
+        self.transform=transform
+        self.Dtrans=Dtrans
+
+    def __call__(self,x):
+        return self.kde(self.transform(x))*np.abs(self.Dtrans(x))
+
+def get_kde_bootstrap(data,evalpoints,nsamples,bw,transform=lambda x: x, Dtrans = lambda x: 1):
     estimates=np.zeros([len(evalpoints),nsamples])
     for i in range(nsamples):
         xstar=np.random.choice(data,len(data))
-        dstar=gaussian_kde(xstar,bw_method=bw)
+        dstar=transformed_kde(xstar,bw_method=bw,transform=transform,Dtrans=Dtrans)
         estimates[:,i]=dstar(evalpoints)
     return estimates
+
