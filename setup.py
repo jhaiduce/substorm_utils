611fc7dc2fc06671bb6dc77e6d6e31779e930079
John Haiducek <jhaiduce@gmail.com>
2019-06-25 16:46:58 -0400

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,25 @@
 from setuptools import find_packages
 from setuptools import setup
 
+# List of dependencies installed via `pip install -e .`
+# by virtue of the Setuptools `install_requires` value below.
+requires = [
+    'numpy',
+    'scipy',
+    'spacepy',
+    'matplotlib',
+    'pytz',
+    'cdaweb@git+https://github.com/jhaiduce/cdaweb',
+    'backports.functools_lru_cache',
+    'cache_decorator@git+https://github.com/jhaiduce/cache_decorator'
+]
+
 setup(
     author='John Haiducek',
     author_email='jhaiduce@umich.edu',
     version='0.0.1',
     zip_safe=True,
     packages=find_packages(),
-    name='substorm_utils'
+    name='substorm_utils',
+    install_requires=requires
 )
